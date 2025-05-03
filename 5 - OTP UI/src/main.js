5efb492b7dd7f9c465f0859557a6df4cbbd63f74
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-03 13:16:39 +0545

    feat: Get references to OTP form DOM elements

---
+++ 5 - OTP UI/src/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputFields = document.querySelectorAll(".otp__input-field");
+    const verifyButton = document.querySelector(".otp__verify-button");
+    const resendCountdown = document.querySelector('.otp__resend-countdown');
+    const timerElement = document.getElementById('timer');
+    const successMessage = document.querySelector('.otp-success');
+    const errorMessage = document.querySelector('.error-message');
+})
