2b6c2ce8fa49f089bae2bd400fb6b11558584017
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-03 15:06:07 +0545

    feat: Add startCountdown function to implement countdown timer logic

--- 5 - OTP UI/src/main.js
+++ 5 - OTP UI/src/main.js
@@ -1,19 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // reference to DOM elements
-    const inputFields = document.querySelectorAll(".otp__input-field");
-    const verifyButton = document.querySelector(".otp__verify-button");
-    const resendButton = document.querySelector('.otp__resend-button');
-    const resendCountdown = document.querySelector('.otp__resend-countdown');
-    const timerElement = document.getElementById('timer');
-    const successMessage = document.querySelector('.otp-success');
-    const errorMessage = document.querySelector('.error-message');
+  // reference to DOM elements
+  const inputFields = document.querySelectorAll(".otp__input-field");
+  const verifyButton = document.querySelector(".otp__verify-button");
+  const resendButton = document.querySelector(".otp__resend-button");
+  const resendCountdown = document.querySelector(".otp__resend-countdown");
+  const timerElement = document.getElementById("timer");
+  const successMessage = document.querySelector(".otp-success");
+  const errorMessage = document.querySelector(".error-message");
 
-    // set initial display to none
-    successMessage.style.display = "none";
-    errorMessage.style.display = "none";
+  // set initial display to none
+  successMessage.style.display = "none";
+  errorMessage.style.display = "none";
 
-    // function to start and reset the countdown timer
-    const startCountdown = () => {
-        
-    }
-})
+  // timer
+  let timerInterval;
+  let remainingTime = 60;
+
+  // function to start and reset the countdown timer
+  const startCountdown = () => {
+    resendButton.style.display = "none";
+    resendCountdown.style.display = "block";
+
+    remainingTime = 60;
+    timerElement.textContent = remainingTime;
+
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+      remainingTime--;
+      timerElement.textContent = remainingTime;
+
+      if (remainingTime <= 0) {
+        clearInterval(timerInterval);
+        resendButton.style.displya = "block";
+        resendCountdown.style.display = "none";
+      }
+    }, 1000);
+  };
+
+  startCountdown();
+});
