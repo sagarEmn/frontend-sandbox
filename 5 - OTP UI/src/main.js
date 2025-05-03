f13bed4a193491462bf393bce91d29285df90bb4
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-03 13:28:47 +0545

    fix: Replace div with button for better semantics

--- 5 - OTP UI/src/main.js
+++ 5 - OTP UI/src/main.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // reference to DOM elements
     const inputFields = document.querySelectorAll(".otp__input-field");
     const verifyButton = document.querySelector(".otp__verify-button");
     const resendCountdown = document.querySelector('.otp__resend-countdown');
     const timerElement = document.getElementById('timer');
     const successMessage = document.querySelector('.otp-success');
     const errorMessage = document.querySelector('.error-message');
+
+    // set initial display to none
+    successMessage.style.display = "none";
+    errorMessage.style.display = "none";
+
+    // function to start and reset the countdown timer
+    const startCountdown = () => {
+        
+    }
 })
