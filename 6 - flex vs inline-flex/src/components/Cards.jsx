85966e277f899b46263db2a9aa45969d25ea2659
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-31 18:08:51 +0545

    feat: Better stylings, change title of the document

--- 6 - flex vs inline-flex/src/components/Cards.jsx
+++ 6 - flex vs inline-flex/src/components/Cards.jsx
@@ -1,7 +1,7 @@
 export const Cards = () => {
-    return(
-        <div className="border-8 border-red-400 bg-red-400 w-32 h-32 grid place-items-center">
-            <p className="text-center">Card</p>
-        </div>
-    )
-}
+  return (
+    <div className="rounded-2xl border-8 border-red-400 bg-red-400/40 w-32 h-32 grid place-items-center">
+      <p className="text-center font-bold text-2xl">Card</p>
+    </div>
+  );
+};
