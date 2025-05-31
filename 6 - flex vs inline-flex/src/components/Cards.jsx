d2b1ef5443c5dd3ad392d63f68e53d0c6cac3223
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-31 17:57:38 +0545

    feat: Center the Card text

--- 6 - flex vs inline-flex/src/components/Cards.jsx
+++ 6 - flex vs inline-flex/src/components/Cards.jsx
@@ -1,7 +1,7 @@
 export const Cards = () => {
     return(
-        <div className="border-8 border-red-400 w-32 h-32">
-            
+        <div className="border-8 border-red-400 bg-red-400 w-32 h-32 grid place-items-center">
+            <p className="text-center">Card</p>
         </div>
     )
 }
